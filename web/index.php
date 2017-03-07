aa042d8f6d40264489543b935b8ff4c6b0b6770b
dfimiarz <dfimiarz@ccny.cuny.edu>
2017-03-07 15:38:37 -0500

    Basic application structure - further tweaks p2

--- web/index.php
+++ web/index.php
@@ -2,4 +2,16 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-echo "CURES cures";
+$app = new \Silex\Application();
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../ccny/scidiv/cures/views',
+));
+
+$app->register(new Silex\Provider\AssetServiceProvider(), array());
+
+$app->get("/","ccny\scidiv\cures\ctrl\HomeController::indexAction")->bind("home");
+
+$app->run();
