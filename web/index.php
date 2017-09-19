c3687112a33e85a0defd90a07b4fe72b2830205c
dfimiarz <dfimiarz@ccny.cuny.edu>
2017-09-19 15:26:56 -0400

    Adding some ajax calls for location and request type

--- web/index.php
+++ web/index.php
@@ -15,5 +15,6 @@ $app->register(new Silex\Provider\AssetServiceProvider(), array());
 $app->get("/","ccny\scidiv\cures\ctrl\HomeController::indexAction")->bind("home");
 $app->get("/workorder","ccny\scidiv\cures\ctrl\WorkOrderController::newWorkOrder")->bind("newWorkOrder");
 $app->post("/workorder","ccny\scidiv\cures\ctrl\WorkOrderController::processWorkOrder")->bind("processWorkOrder");
+$app->get("/api/location","ccny\scidiv\cures\ctrl\ApiLocationController::listLocations")->bind("listLocations");
 
 $app->run();
